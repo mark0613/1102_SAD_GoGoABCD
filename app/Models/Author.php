80e1441a30f82792d8e9133b1179e173d2242a43
mark0613 <goldffd700@gmail.com>
2022-05-04 20:55:57 +0800

    update file

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -8,11 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model {
     protected $table = 'author';
     protected $primaryKey = ['p_id', 'name'];
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
 
     public $incrementing = false;
     public $timestamps = false;
