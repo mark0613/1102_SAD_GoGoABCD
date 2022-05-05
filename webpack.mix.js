a7337cd20c5caaa017055e323866be1762990b89
mark0613 <goldffd700@gmail.com>
2022-05-05 19:43:52 +0800

    update file

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,5 +4,5 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ])
-    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/app.scss', 'public/scss')
     .sourceMaps();
