e02d7e3fcf432b273492f76a88578c0f14873198
mark0613 <goldffd700@gmail.com>
2022-05-07 16:25:21 +0800

    samll window

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+$( document ).ready(function() {
+    // date picker
+    $('.datepicker').datepicker({
+        format: 'yyyy-mm-dd',
+    });
+    $('.multi-select').selectpicker();
+});
