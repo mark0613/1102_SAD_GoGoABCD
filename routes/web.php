66808cc762c53ce672872b5edef7ea3399cf2f77
mark0613 <goldffd700@gmail.com>
2022-05-07 16:29:32 +0800

    add route: admin/product

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserAuthController;
 
@@ -10,9 +12,9 @@ Route::get('/', function () {
 
 // Route::get('/', [HomeController::class, "indexPage"]);
 
-// user/auth/...
-Route::group(['prefix' => 'user'], function(){
-    Route::group(['prefix' => 'auth'], function(){
+// user/auth/*
+Route::group(['prefix' => 'user'], function() {
+    Route::group(['prefix' => 'auth'], function() {
         Route::get('/register', [UserAuthController::class, 'registerPage']);
         Route::post('/register', [UserAuthController::class, 'registerUserProcess']);
         Route::get('/login', [UserAuthController::class, 'loginPage']);
@@ -20,4 +22,11 @@ Route::group(['prefix' => 'user'], function(){
         Route::get('/logout', [UserAuthController::class, 'logout']);
     });
 });
+
+// admin/*
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('/product', [AdminController::class, 'productPage']);
+    Route::post('/product', [AdminController::class, 'productProcess']);
+})
+
 ?>
