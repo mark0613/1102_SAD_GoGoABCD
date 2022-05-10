8e4b39ef0a5ba6a7a02c5f472ca33aeefe265df5
mark0613 <goldffd700@gmail.com>
2022-05-10 16:48:16 +0800

    update file

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,30 +1,20 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-
     .js('resources/js/merchant.js', 'public/js')
-
     .js('resources/js/customer.js', 'public/js')
-
     .postCss('resources/css/app.css', 'public/css', [
         //
     ])
-
     .postCss('resources/css/base.css', 'public/css', [
     ])
-
     .postCss('resources/css/home.css', 'public/css', [
     ])
-
     .postCss('resources/css/merchant.css', 'public/css', [
     ])
-
     .postCss('resources/css/customer.css', 'public/css', [
     ])
-
     .sass('resources/sass/app.scss', 'public/css')
-
     .sass('resources/sass/home.scss', 'public/css')
-
     .sourceMaps();
 
